dbc2762f871b5f4e40d19d5e31033920841d424d
GuidiSara <guidi.saretta97@gmail.com>
2022-04-22 09:47:26 +0200

    inizialized project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Challenge Bubble</h1>
+  <div class="cartesianPlane">
+    <div id="svgContainer"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import * as d3 from "d3";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {},
+  mounted() {
+    const width = 150;
+    const height = 150;
+    let svg1 = d3.select("#svgcontainer").append("svg").attr("width", width).attr("height", height);
+    svg1.append("circle").attr("cx", 100).attr("cy", 100).attr("r", 30).attr("fill", "purple");
+    let svg2 = d3.select("#svgcontainer").append("svg").attr("width", width).attr("height", height);
+    svg2.append("circle").attr("cx", 30).attr("cy", 30).attr("r", 30).attr("fill", "purple");
+    let svg3 = d3.select("#svgcontainer").append("svg").attr("width", width).attr("height", height);
+    svg3.append("circle").attr("cx", 70).attr("cy", 70).attr("r", 30).attr("fill", "purple");
+  },
+};
 </script>
 
 <style>
-#app {
+h1 {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: red;
   margin-top: 60px;
+  margin-bottom: 100px !important;
 }
 </style>
