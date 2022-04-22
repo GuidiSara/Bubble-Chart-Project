653b568c1a5d0057ba64131fb338c88088735541
GuidiSara <guidi.saretta97@gmail.com>
2022-04-22 16:16:35 +0200

    added transition

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <h1>Challenge Bubble</h1>
   <div class="cartesianPlane">
-    <div id="svgContainer"></div>
+    <svg width="8000" height="800"></svg>
     <button @click="changeX()">Cambia x</button>
-    <button>Cambia y</button>
+    <button @click="changeY()">Cambia y</button>
   </div>
 </template>
 
@@ -14,8 +14,9 @@ export default {
   name: "App",
   data: function () {
     return {
-      circles: "",
       svg: "",
+      clickX: false,
+      clickY: false,
     };
   },
 
@@ -23,21 +24,89 @@ export default {
     this.createdCircles();
   },
   methods: {
+    //created circles
     createdCircles() {
-      this.circles = quantitativeVariablesCircle.circles;
-      this.svg = d3.select("#svgContainer").append("svg").attr("width", 8000).attr("height", 800);
-      this.circles.forEach((circle) => {
-        this.svg
-          .append("circle")
-          .attr("cx", circle.v1)
-          .attr("cy", circle.v2)
-          .attr("r", circle.v3)
-          .attr("stroke", "#5e9f4d")
-          .attr("fill", "#b7d2a9");
-      });
+      let dataSet = quantitativeVariablesCircle.circles;
+      this.svg = d3.select("svg");
+      this.svg
+        .selectAll("circle")
+        .data(dataSet)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) {
+          return d.v1;
+        })
+        .attr("cy", function (d) {
+          return d.v2;
+        })
+        .attr("r", function (d) {
+          return d.v3;
+        })
+        .attr("stroke", "#5e9f4d")
+        .attr("fill", "#b7d2a9");
     },
     changeX() {
-      console.log(this.svg);
+      console.log(this.clickX);
+      if (this.clickX) {
+        d3.select("svg")
+          .selectAll("circle")
+          .attr("cx", function (d) {
+            return d.v1;
+          })
+          .transition()
+          .duration(1000)
+          .attr("r", function (d) {
+            return d.v3;
+          })
+          .attr("stroke", "#5e9f4d")
+          .attr("fill", "#b7d2a9");
+        this.clickX = false;
+      } else {
+        d3.select("svg")
+          .selectAll("circle")
+          .attr("cx", function (d) {
+            return d.v3;
+          })
+          .transition()
+          .duration(1000)
+          .attr("r", function (d) {
+            return d.v1;
+          })
+          .attr("stroke", "#5e9f4d")
+          .attr("fill", "#b7d2a9");
+        this.clickX = true;
+      }
+    },
+    changeY() {
+      if (this.clickY) {
+        d3.select("svg")
+          .selectAll("circle")
+          .attr("cy", function (d) {
+            return d.v2;
+          })
+          .transition()
+          .duration(1000)
+          .attr("r", function (d) {
+            return d.v3;
+          })
+          .attr("stroke", "#5e9f4d")
+          .attr("fill", "#b7d2a9");
+        this.clickY = false;
+      } else {
+        d3.select("svg")
+          .selectAll("circle")
+          .attr("cy", function (d) {
+            return d.v3;
+          })
+          .transition()
+          .duration(1000)
+          .attr("r", function (d) {
+            return d.v2;
+          })
+          .attr("stroke", "#5e9f4d")
+          .attr("fill", "#b7d2a9");
+        this.clickY = true;
+      }
     },
   },
 };
